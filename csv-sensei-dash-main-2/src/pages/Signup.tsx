2c3322dbc953bd663a356b890aba43de35cef197
Aditya Meena <adityameena2233@gmail.com>
2025-10-14 02:42:18 +0530

    fixed the toggle

--- csv-sensei-dash-main-2/src/pages/Signup.tsx
+++ csv-sensei-dash-main-2/src/pages/Signup.tsx
@@ -316,7 +316,7 @@ const Signup = () => {
               <p className="text-sm text-gray-600 dark:text-gray-400">
                 Already have an account?{' '}
                 <Link 
-                  to="/" 
+                  to="/login" 
                   className="text-blue-600 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 font-medium hover:underline"
                 >
                   Sign in
